695c267edccd11a81bcedfedc530e5b48edde7e1
Andy Ansong <andyansong82@gmail.com>
2025-03-24 22:28:35 +0000

    config android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.paypilot.app',
+  appName: 'PayPilot',
+  webDir: 'dist/paypilot/browser'
+};
+
+export default config;
